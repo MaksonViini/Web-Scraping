58bd1a30ef9af99cf02e4d2c666503c45a4900ab
MaksonViini <maksonvinicio7@gmail.com>
2021-03-29 21:58:37 -0300

    Subindo codigo so scrapy completo, junto do arquivo de crawl pagination

--- MercadoLivre/MercadoLivre/spiders/ml.py
+++ MercadoLivre/MercadoLivre/spiders/ml.py
@@ -1,4 +1,6 @@
+from urllib import parse
 import scrapy
+
 
 class MercadoLivreSpider(scrapy.Spider):
     name = 'ml'
@@ -9,8 +11,13 @@ class MercadoLivreSpider(scrapy.Spider):
 
     def parse(self, response):
         for i in response.xpath('//li[@class="promotion-item"]'):
-            price = i.xpath('.//span[@class="promotion-item__description"]//text()').getall()
-            title = i.xpath('.//p[@class="promotion-item__title"]//text()').get()
+
+            price = i.xpath(
+                './/span[@class="promotion-item__price"]//text()').getall()
+
+            title = i.xpath(
+                './/p[@class="promotion-item__title"]//text()').get()
+
             link = i.xpath('./a/@href').get()
 
             yield {
@@ -18,3 +25,8 @@ class MercadoLivreSpider(scrapy.Spider):
                 'title': title,
                 'link': link
             }
+
+            next_page = response.xpath(
+                '//a[contains(@title,"Próxima")]/@href').get()
+            if next_page:
+                yield scrapy.Request(url=next_page, callback=self.parse)
