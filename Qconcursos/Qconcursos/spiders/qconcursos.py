d72d2771ea4a7ccf9f14e191b1857a153965b81d
MaksonViini <maksonvinicio7@gmail.com>
2021-04-14 15:57:32 -0300

    Projeto de crawl no site Qconcursos

--- Qconcursos/Qconcursos/spiders/qconcursos.py
+++ Qconcursos/Qconcursos/spiders/qconcursos.py
@@ -8,12 +8,17 @@ class QconcursosSpider(scrapy.Spider):
         'https://www.qconcursos.com/questoes-de-concursos/questoes'
     ]
 
-    def parse(self, reponse):
-        quest = xpath()
-        enunciated = xpath()
-        answer = xpath()
-
+    def parse(self, response):
+        for i in response.xpath('//div[@class="q-questions-list js-questions-list"]'):
+            id_quest = i.xpath('//span[@class="q-index js-question-index visible"]//text()')
+            enunciated = i.xpath(
+                '//div[@class="q-question-enunciation"]/span//text()').get()
+            questions = i.xpath()
+            answer = i.xpath()
 
         yield {
-
+            'id_quest': id_quest,
+            'enunciated': enunciated,
+            'questions': questions,
+            'answer': answer
         }
